cca29e91b5655b6bbdc857c18b770fef948a312d
jgeurten <jjgeurte@edu.uwaterloo.ca>
2018-12-04 03:20:34 -0500

    Last of monday commits. Time to start simulating

--- calcGoalPOs.m
+++ calcGoalPOs.m
@@ -9,6 +9,11 @@ AREA = 500;
 THRESH = 2000; % threshold for difference operator
 PUCK_MIN_AREA = 100; 
 MED_FILT_SIZE = 7; 
+PXS_PER_M = 320;
+
+R_12 = [-1 74 14]; % (cm) World vector describing bottom left corner to top left
+R_23 = [166 8 0]; 
+R_13 = R_12+R_23; 
 
 %disp('Cropping Final Position Video ...')
 v = VideoReader('Shot_0.MP4'); 
@@ -67,9 +72,6 @@ end
 
 
 % Find the puck contact location
-% red_frames = zeros(v.Height, v.Width, round(v.Duration*v.FrameRate), 'uint8'); 
-% green_frames = zeros(v.Height, v.Width, round(v.Duration*v.FrameRate), 'uint8'); 
-% blue_frames = zeros(v.Height, v.Width, round(v.Duration*v.FrameRate), 'uint8'); 
 count = 1;
 
 disp('Locating the Puck ...');
@@ -79,7 +81,7 @@ sums_green = zeros(nFrames - 10, 1);
 sums_blue = zeros(nFrames - 10, 1);
 count = 1; 
 % Calculate first differences
-for i = 140:160%nFrames -10
+for i = 1:nFrames -10
     testFrames_cur = read(v, i); 
     testFrames_next = read(v, i+1); 
     red_frame_diffs = testFrames_next(:,:,1) - testFrames_cur(:,:,1); 
@@ -123,5 +125,8 @@ rectangle('Position', [PUCK_REGION.BoundingBox(1),PUCK_REGION.BoundingBox(2),...
     PUCK_REGION.BoundingBox(3),PUCK_REGION.BoundingBox(4)],  'EdgeColor','r','LineWidth',2 ) 
 
 puck_centroid = PUCK_REGION.Centroid; 
+R_puck_1 = puck_centroid - bottom_left.centroid; 
+R_puck_2 = puck_centroid - left_corner.centroid; 
+R_puck_3 = puck_centroid - right_corner.centroid; 
 
 
